4593c67d991c8a1ff27ad40f915c9cfda5e282f4
katianne40@gmail.com <katianne40@gmail.com>
2024-11-07 09:19:51 -0300

    feat: ensure transaction can be create

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
@@ -11,14 +13,21 @@ import { MoneyInput } from "./money-input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { Transaction_Category_Option, Transaction_Payment_Method_Options, Transaction_type_option } from "../_constants/transactions";
 import { DatePicker } from "./ui/date-picker";
+import { addTransaction } from "../_actions/add-transaction";
+import { useState } from "react";
+
 
 const formSchema = z.object({
     name: z.string().trim().min(1, {
         message: "O nome é obrigatório.",
     }),
-    amount: z.string().trim().min(1, {
-        message: "O valor é obrigatório.",
-    }),
+    amount: z
+        .number({
+            required_error: "O valor é obrigatório.",
+        }).positive({
+            message: "O valor deve ser positivo.",
+        }),
+
     type: z.nativeEnum(TransactionType, {
         required_error: "O tipo é obrigatório.",
     }),
@@ -35,11 +44,13 @@ const formSchema = z.object({
 
 type FormSchema = z.infer<typeof formSchema>
 
-const AddTransactionButton = () => {
+
+const AddTransactionButton: React.FC = () => {
+    const [dialogIsOpen, setDialogIsOpen] = useState(false)
     const form = useForm<FormSchema>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            amount: "",
+            amount: 50,
             category: TransactionCategory.OTHER,
             date: new Date(),
             name: "",
@@ -48,16 +59,27 @@ const AddTransactionButton = () => {
         },
     });
 
-    const onSubmit = (data: FormSchema) => {
-        console.log(data)
+
+    const onSubmit = async (data: FormSchema) => {
+        try {
+            await addTransaction(data);
+            setDialogIsOpen(false);
+            form.reset();
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
-        <Dialog onOpenChange={(open) => {
-            if (!open) {
-                form.reset();
-            }
-        }}>
+
+        <Dialog
+            open={dialogIsOpen}
+            onOpenChange={(open) => {
+                setDialogIsOpen(open);
+                if (!open) {
+                    form.reset();
+                }
+            }}>
             <DialogTrigger asChild>
                 <Button className="rounded-full font-bold">
                     Adicionar Transações
@@ -94,7 +116,16 @@ const AddTransactionButton = () => {
                                 <FormItem>
                                     <FormLabel>Valor</FormLabel>
                                     <FormControl>
-                                        <MoneyInput placeholder="Digite o valor..." {...field} />
+                                        <MoneyInput
+                                            placeholder="Digite o valor..."
+                                            value={field.value}
+                                            onValueChange={({ floatValue }) =>
+                                                field.onChange(floatValue)
+                                            }
+                                            onBlur={field.onBlur}
+                                            disabled={field.disabled}
+
+                                        />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
@@ -192,9 +223,6 @@ const AddTransactionButton = () => {
                                 </FormItem>
                             )}
                         />
-
-                        <Button type="submit">Submit</Button>
-
                         <DialogFooter>
                             <DialogClose asChild>
                                 <Button type="button" variant="outline">
